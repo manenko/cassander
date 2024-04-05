df0d2fbbe9b69bacdfeb3c5704bf96c16fc78715
Oleksandr Manenko <koornacht@use.startmail.com>
2024-04-05 18:41:05 +0200

    Add CQL blob type

--- src/cql.rs
+++ src/cql.rs
@@ -1,12 +1,14 @@
 //! CQL types and utilities.
 
 mod ascii;
+mod blob;
 mod inet;
 mod uuid;
 mod uuid_gen;
 mod varint;
 
 pub use ascii::*;
+pub use blob::*;
 pub use inet::*;
 pub use uuid::*;
 pub use uuid_gen::*;
