28a91e1a7f5aa4ce23c63fb844eff4f4ba5ed948
Oleksandr Manenko <koornacht@use.startmail.com>
2024-04-05 16:49:14 +0200

    Add CQL ASCII type

--- src/cql.rs
+++ src/cql.rs
@@ -1,9 +1,11 @@
 //! CQL types and utilities.
 
+mod ascii;
 mod inet;
 mod uuid;
 mod uuid_gen;
 
+pub use ascii::*;
 pub use inet::*;
 pub use uuid::*;
 pub use uuid_gen::*;
---
+++ src/cql/ascii.rs
@@ -0,0 +1,103 @@
+use std::str::FromStr;
+
+use thiserror::Error;
+
+/// An error that occurs when the given string is not a valid ASCII string.
+#[derive(Debug, Clone, PartialEq, Eq, Error)]
+#[error("the given string is not a valid ASCII string: '{0}'")]
+pub struct NonAsciiStringError(String);
+
+impl NonAsciiStringError {
+    /// Creates a new error with the given string which is invalid ASCII string.
+    pub fn new<S>(value: S) -> Self
+    where
+        S: Into<String>,
+    {
+        Self(value.into())
+    }
+}
+
+/// A Cassandra Query Language (CQL) ASCII string.
+#[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub struct CqlAscii(String);
+
+impl CqlAscii {
+    /// Creates a new `CqlAscii` from the given string.
+    ///
+    /// Returns an error if the given string is not a valid ASCII string.
+    pub fn new<S>(value: S) -> Result<Self, NonAsciiStringError>
+    where
+        S: Into<String>,
+    {
+        let value = value.into();
+        if !value.is_ascii() {
+            return Err(NonAsciiStringError::new(value));
+        }
+
+        Ok(Self(value))
+    }
+
+    /// Returns the inner string.
+    pub fn as_str(&self) -> &str {
+        &self.0
+    }
+}
+
+impl AsRef<str> for CqlAscii {
+    /// Returns the inner string as a reference.
+    fn as_ref(&self) -> &str {
+        self.as_str()
+    }
+}
+
+impl AsRef<String> for CqlAscii {
+    /// Returns the inner string as a reference.
+    fn as_ref(&self) -> &String {
+        &self.0
+    }
+}
+
+impl FromStr for CqlAscii {
+    type Err = NonAsciiStringError;
+
+    /// Parses a string into a `CqlAscii`.
+    ///
+    /// Retrun
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Self::new(s)
+    }
+}
+
+impl TryFrom<String> for CqlAscii {
+    type Error = NonAsciiStringError;
+
+    /// Converts a string into a `CqlAscii`.
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        Self::new(value)
+    }
+}
+
+impl TryFrom<&str> for CqlAscii {
+    type Error = NonAsciiStringError;
+
+    /// Converts a string into a `CqlAscii`.
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        Self::new(value)
+    }
+}
+
+impl TryFrom<&String> for CqlAscii {
+    type Error = NonAsciiStringError;
+
+    /// Converts a string into a `CqlAscii`.
+    fn try_from(value: &String) -> Result<Self, Self::Error> {
+        Self::new(value)
+    }
+}
+
+impl From<CqlAscii> for String {
+    /// Converts a `CqlAscii` into a string.
+    fn from(value: CqlAscii) -> Self {
+        value.0
+    }
+}
