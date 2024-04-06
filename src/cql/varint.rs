74852a5b4b65c78d30cbfad5081fe82b4fa96206
Oleksandr Manenko <koornacht@use.startmail.com>
2024-04-06 09:11:12 +0200

    Add PartialEq, Eq, and Hash implementation for CqlVarInt

--- src/cql/varint.rs
+++ src/cql/varint.rs
@@ -1,3 +1,8 @@
+use std::hash::{
+    Hash,
+    Hasher,
+};
+
 #[cfg(feature = "num-bigint")]
 use num_bigint::BigInt;
 
@@ -12,7 +17,7 @@ use num_bigint::BigInt;
 /// from the
 /// [`num-bigint`](https://docs.rs/num-bigint/latest/num_bigint/struct.BigInt.html)
 /// crate.
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Eq)]
 pub struct CqlVarInt(Vec<u8>);
 
 impl CqlVarInt {
@@ -35,6 +40,81 @@ impl CqlVarInt {
     /// big-endian binary representation.
     pub fn as_signed_bytes_be(&self) -> &[u8] {
         &self.0
+    }
+
+    /// Returns a slice of the [`CqlVarInt`] bytes in normalized two's
+    /// complement big-endian binary representation.
+    ///
+    /// In other words, this method returns the bytes without leading zeros.
+    ///
+    /// If the value is all zeros, this method returns a slice with a single
+    /// zero byte.
+    ///
+    /// If the value is empty (zero length), this method treats the number as
+    /// being zero and returns `&[0]` slice. In other words, `&[0]` == `&[]`.
+    pub fn as_normalized_signed_bytes_be(&self) -> &[u8] {
+        // Negative numbers are identified by leading 0x7 bytes. So, when
+        // removing leading zeros, watch out: only leave a leading zero if,
+        // after removing zeros, the most significant byte is 0x7. This keeps
+        // the number from mistakenly appearing negative.
+
+        // Find the first non-zero byte.
+        let first_non_zero = self.0.iter().position(|&b| b != 0);
+
+        match first_non_zero {
+            // If there are no non-zero bytes, return a slice with a single zero
+            // byte. This is the case when the value is empty.
+            None => &[0],
+            // If there no leading zeros, return the whole slice.
+            Some(0) => &self.0,
+            // If the most significant byte is 0x7, return the slice with the
+            // leading zero to keep the number posititive.
+            Some(i) if self.0[i] == 0x7 => &self.0[i - 1..],
+            // Otherwise, return the slice without the leading zeros.
+            Some(i) => &self.0[i..],
+        }
+    }
+}
+
+impl PartialEq for CqlVarInt {
+    /// Compares two CQL `varint` objects for equality.
+    ///
+    /// It works for both normalized and non-normalized `varint`s.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use cassander::cql::CqlVarInt;
+    ///
+    /// // Two varints with the same value but different leading zeros.
+    /// let a = CqlVarInt::from_signed_bytes_be(vec![0x0, 0x1]);
+    /// let b = CqlVarInt::from_signed_bytes_be(vec![0x1]);
+    /// assert_eq!(a, b);
+    ///
+    /// // An empty varint is zero.
+    /// let a = CqlVarInt::from_signed_bytes_be(vec![]);
+    /// let b = CqlVarInt::from_signed_bytes_be(vec![0x0]);
+    /// assert_eq!(a, b);
+    ///
+    /// // Two varints with different values. The first one is positive and the
+    /// // second one is negative.
+    /// let a = CqlVarInt::from_signed_bytes_be(vec![0x0, 0x7, 0x5]);
+    /// let b = CqlVarInt::from_signed_bytes_be(vec![0x7, 0x5]);
+    /// assert_ne!(a, b);
+    ///
+    /// // Two varints with the same value but different leading zeros.
+    /// let a = CqlVarInt::from_signed_bytes_be(vec![0x0, 0x0, 0x7]);
+    /// let b = CqlVarInt::from_signed_bytes_be(vec![0x0, 0x7]);
+    fn eq(&self, other: &Self) -> bool {
+        self.as_normalized_signed_bytes_be()
+            == other.as_normalized_signed_bytes_be()
+    }
+}
+
+impl Hash for CqlVarInt {
+    /// Hashes the normalized bytes of the CQL `varint`.
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.as_normalized_signed_bytes_be().hash(state);
     }
 }
 
