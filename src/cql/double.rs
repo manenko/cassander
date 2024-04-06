abd965abc49652d767aabf1d4b61cbd9d38695a5
Oleksandr Manenko <koornacht@use.startmail.com>
2024-04-07 00:08:00 +0200

    Add CQL float and double types

---
+++ src/cql/double.rs
@@ -0,0 +1,36 @@
+/// A CQL `double` type.
+#[derive(Debug, Clone, PartialEq, PartialOrd)]
+pub struct CqlDouble(f64);
+
+impl CqlDouble {
+    /// Creates a new [`CqlDouble`] from the given [`f64`] value.
+    pub fn new(value: f64) -> Self {
+        Self(value)
+    }
+
+    /// Returns the inner [`f64`] value.
+    pub fn as_f64(&self) -> f64 {
+        self.0
+    }
+}
+
+impl From<f64> for CqlDouble {
+    /// Converts the given [`f64`] into a [`CqlDouble`].
+    fn from(value: f64) -> Self {
+        Self::new(value)
+    }
+}
+
+impl From<CqlDouble> for f64 {
+    /// Converts the given [`CqlDouble`] into an [`f64`].
+    fn from(value: CqlDouble) -> Self {
+        value.as_f64()
+    }
+}
+
+impl AsRef<f64> for CqlDouble {
+    /// Returns the inner [`f64`] value as a reference.
+    fn as_ref(&self) -> &f64 {
+        &self.0
+    }
+}
---
+++ src/cql/float.rs
@@ -0,0 +1,36 @@
+/// A CQL `float` type.
+#[derive(Debug, Clone, PartialEq, PartialOrd)]
+pub struct CqlFloat(f32);
+
+impl CqlFloat {
+    /// Creates a new [`CqlFloat`] from the given [`f32`] value.
+    pub fn new(value: f32) -> Self {
+        Self(value)
+    }
+
+    /// Returns the inner [`f32`] value.
+    pub fn as_f32(&self) -> f32 {
+        self.0
+    }
+}
+
+impl From<f32> for CqlFloat {
+    /// Converts the given [`f32`] into a [`CqlFloat`].
+    fn from(value: f32) -> Self {
+        Self::new(value)
+    }
+}
+
+impl From<CqlFloat> for f32 {
+    /// Converts the given [`CqlFloat`] into an [`f32`].
+    fn from(value: CqlFloat) -> Self {
+        value.as_f32()
+    }
+}
+
+impl AsRef<f32> for CqlFloat {
+    /// Returns the inner [`f32`] value as a reference.
+    fn as_ref(&self) -> &f32 {
+        &self.0
+    }
+}
