c57bb79b8817fa00dde091057009da8b2c5ebb1c
Oleksandr Manenko <koornacht@use.startmail.com>
2024-04-18 09:41:22 +0200

    Add documentation comments for CQL ValueType enum

--- src/cql/value_type.rs
+++ src/cql/value_type.rs
@@ -33,32 +33,98 @@ use crate::ffi::{
 /// A type of a CQL value.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum ValueType {
+    /// CQL `ascii` type, which represents strings with only ASCII characters.
     Ascii,
+    /// CQL `bigint` type, which represents 64-bit signed integer numbers.
     BigInt,
+    /// CQL `blob` type, which represents arbitrary binary data.
     Blob,
+    /// CQL `boolean` type, which represents boolean values.
     Boolean,
+    /// CQL `counter` type, which represents 64-bit signed integer numbers that
+    /// are incremented or decremented.
+    ///
+    /// Note that the value of a counter cannot be set: a counter does not
+    /// exist until first incremented/decremented, and that first
+    /// increment/decrement is made as if the prior value was 0.
+    ///
+    /// See [Cassandra documention](https://cassandra.apache.org/doc/stable/cassandra/cql/types.html#counters)
+    /// for more information.
     Counter,
+    /// A custom type is a string that contains the name of Java class that
+    /// extends the server side `AbstractType` class and that can be loaded by
+    /// Cassandra.
+    ///
+    /// <div class="warning">
+    /// Custom types exists mostly for backward compatibility purposes and their
+    /// usage is discouraged. Their usage is complex, not user friendly and the
+    /// other provided types, particularly user-defined types, should almost
+    /// always be enough.
+    /// </div>
+    ///
+    /// See [Cassandra documention](https://cassandra.apache.org/doc/stable/cql/types.html#custom-types)
+    /// for more information.
     Custom,
+    /// CQL `date` type, which represents a date without a time component.
     Date,
+    /// CQL `decimal` type, which represents arbitrary precision decimal numbers.
     Decimal,
+    /// CQL `double` type, which represents 64-bit floating point numbers.
     Double,
+    /// CQL `duration` type, which represents a period of time with nanoseconds
+    /// precision.
     Duration,
+    /// CQL `float` type, which represents 32-bit floating point numbers.
     Float,
+    /// CQL `inet` type, which represents an IPv4 or IPv6 address.
     Inet,
+    /// CQL `int` type, which represents 32-bit signed integer numbers.
     Int,
+    /// CQL `list` type, which represents a (sorted) collection of non-unique
+    /// values where elements are ordered by their position in the list.
     List,
+    /// CQL `map` type, which represents a (sorted) set of key-value pairs,
+    /// where keys are unique and the map is sorted by its keys.
     Map,
+    /// CQL `set` type, which represents a (sorted) collection of unique values.
     Set,
+    /// CQL `smallint` type, which represents 16-bit signed integer numbers.
     SmallInt,
+    /// CQL `text` type, which represents strings with UTF-8 encoding.
     Text,
+    /// CQL `time` type, which represents a time of day as the number of
+    /// nanoseconds since midnight.
     Time,
+    /// CQL `timeuuid` type, which represents a type 1 UUID, which is a
+    /// combination of a timestamp and a unique identifier.
+    ///
+    /// It is generally used as a conflict-free timestamp.
     TimeUuid,
+    /// CQL `timestamp` type, which represents a date and time with millisecond
+    /// precision.
     Timestamp,
+    /// CQL `tinyint` type, which represents 8-bit signed integer numbers.
     TinyInt,
+    /// CQL `tuple` type, which represents a fixed-size collection of elements
+    /// where each element can have a different type.
+    ///
+    /// Functionally, tuples can be though as anonymous UDT with anonymous
+    /// fields.
     Tuple,
+    /// CQL user-defined type aka UDT.
+    ///
+    /// A UDT has a name (used to declare columns of that type) and is a set of
+    /// named and typed fields. Fields name can be any type, including
+    /// collections or other UDT.
+    ///
+    /// See [Cassandra documention](https://cassandra.apache.org/doc/stable/cql/types.html#udts)
+    /// for more information.
     Udt,
+    /// CQL `uuid` type, which represents a type 1 or type 4 UUID.
     Uuid,
+    /// CQL `varchar` type, which represents strings with UTF-8 encoding.
     VarChar,
+    /// CQL `varint` type, which represents arbitrary precision integer numbers.
     VarInt,
 }
 
