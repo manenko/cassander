2b8d87a0e2cb4a2273a47734c1eb2637951593a9
Oleksandr Manenko <koornacht@use.startmail.com>
2025-04-27 20:11:50 +0200

    Update FFI bindings

--- generate_bindings.sh
+++ generate_bindings.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 bindgen --c-naming \
-	--enable-function-attribute-detection \
-	--must-use-type CassError \
-	--must-use-type CassErrorResult \
-	--must-use-type CassFuture \
-	--output src/driver/ffi/cassandra.rs \
-	src/driver/ffi/wrapper.h
+  --enable-function-attribute-detection \
+  --must-use-type CassError \
+  --must-use-type CassErrorResult \
+  --must-use-type CassFuture \
+  --output src/ffi/cassandra.rs \
+  src/ffi/wrapper.h
